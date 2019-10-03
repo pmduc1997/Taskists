2b37e247a0cd55d3864f353df246ed4c0140f3c1
unknown <pmduc1997@gmail.com>
2019-10-03 08:29:04 +0700

    test

--- Taskists/app/containers/HomePage/index.js
+++ Taskists/app/containers/HomePage/index.js
@@ -14,6 +14,7 @@ import SiderStyle from '../../components/Sider/index';
 export default function HomePage() {
   return (
     <SiderStyle>
+      <CardStyle></CardStyle>
     </SiderStyle>
   );
 }
