f74e145679248931b71e9302b1f4dd9c3ebab364
QuanTruong <intern.tbquan@cmc.com.vn>
2019-10-04 09:57:27 +0700

    fix list User

--- Taskists/app/components/Table/index.js
+++ Taskists/app/components/Table/index.js
@@ -1,50 +1,66 @@
 
 import React from 'react';
 // import {columns,data} from './data'
-import { columns, data } from "./dataUser";
-import { Table, Divider, Tag, Input } from 'antd';
-
-function onChange(pagination, filters, sorter) {
-  console.log('params', pagination, filters, sorter);
-}
+// import { columns, data } from "./dataUser";
+import { Table, Button, Tag, Input } from 'antd';
 
 export default class TableStyle extends React.Component {
+  constructor(props) {
+    super(props);
+    this._sttOn = this._sttOn.bind(this)
+  };
+  _sttOn(text) {
+    console.log('TEXT',text)
+  }
   render() {
+
     const columns = [
       {
         title: 'Name',
         dataIndex: 'name',
         render: text => <a>{text}</a>,
+        key: 'name'
       },
       {
         title: 'User Name',
-        dataIndex: 'userName'
+        dataIndex: 'userName',
+        key: 'username'
       },
       {
         title: 'Email',
-        dataIndex: 'email'
+        dataIndex: 'email',
+        key: 'email'
       },
       {
         title: 'Password',
         dataIndex: 'password',
-        render: text => <Input.Password value={text}/>
+        render: text => <Input.Password value={text} />,
+        key: 'password'
       },
       {
         title: 'Role',
-        dataIndex: 'role'
+        dataIndex: 'role',
+        key: 'role'
       },
       {
         title: 'Created At',
-        dataIndex: 'createdAt'
+        dataIndex: 'createdAt',
+        key: 'createdAt'
       },
       {
         title: 'Created By',
-        dataIndex: 'createdBy'
+        dataIndex: 'createdBy',
+        key: 'createdBy'
       },
       {
         title: 'Status',
         dataIndex: 'status',
-      },
+        key: 'status',
+        render: text => {
+          if (text == 'active') return <Button onClick={() => this._sttOn(text)} type="primary">active</Button>
+          else if (text == 'deactive') return <Button type="danger">deactive</Button>
+        }
+      }
     ];
 
     const data = [
@@ -54,67 +70,90 @@ export default class TableStyle extends React.Component {
         email: 'pmduc@gmail.com',
         description: 'HAHA',
         status: 'active',
-        password: 'aaaaaaaaaaaaaaaaa'
+        password: 'aaaaaaaaaaaaaaaaa',
+        role: 'admin',
+        key: '1'
       },
       {
         name: 'Khanh',
         userName: 'Khanh maldoo',
         description: 'HOH',
         status: 'active',
+        key: '2',
+        password: 'aaaaaaaaaaaaaaaaa',
+        role: 'user'
       },
       {
         name: 'HuyMini',
         userName: 'Captun cua Nam Anh',
         description: 'Gay',
-        status: 'active',
+        key: '3',
+        status: 'deactive',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'NamAnh',
         userName: 'NamAnh of Capgay',
+        key: '4',
         description: 'BD',
-        status: 'active',
+        status: 'deactive',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'HuyDD',
+        key: '5',
         userName: 'Huy DD anh toi',
         description: 'HOH',
         status: 'active',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Long',
         userName: 'LongIN',
         description: 'HOH',
-        status: 'active',
+        key: '6',
+        status: 'deactive',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Duong',
         userName: 'DuongGEPONXI',
+        key: '7',
         description: 'HOH',
         status: 'active',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Long',
         userName: 'LongIN',
+        key: '8',
         description: 'HOH',
         status: 'active',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Duong',
         userName: 'DuongGEPONXI',
+        key: '9',
         description: 'HOH',
-        status: 'active',
+        status: 'deactive',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Long',
         userName: 'LongIN',
+        key: '10',
         description: 'HOH',
         status: 'active',
+        password: 'aaaaaaaaaaaaaaaaa'
       },
       {
         name: 'Duong',
+        key: '11',
         userName: 'DuongGEPONXI',
         description: 'HOH',
         status: 'active',
+        password: 'aaaaaaaaaaaaaaaaa'
       }
     ];
     return (
